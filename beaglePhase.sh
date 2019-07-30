ca5ea749378f05cf54868f6ab6dad6892e80ebfc
esohkevin <esohkevin4@gmail.com>
2019-07-30 23:33:10 +0000

    update

--- beaglePhase.sh
+++ beaglePhase.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-java --jar beagle.12Jul19.0df.jar \
-	gt=<input-vcf> \
-	out=<output prefix> \
+if [[ $# == 2 ]]; then
+
+   java -jar beagle.12Jul19.0df.jar \
+	gt=${1} \
+	out=${2} \
 	burnin=10 \
 	iterations=15
+
+else
+   echo """
+	Usage: ./beaglePhase.sh <vcf-file> <out-file>
+   """
+
+fi
