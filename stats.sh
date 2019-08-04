b7ffd0987e0fe68361ab459910b0ee44f9de021d
esohkevin <esohkevin4@gmail.com>
2019-08-04 05:29:08 +0000

    update stats.sh and pipe.sh

--- stats.sh
+++ stats.sh
@@ -3,11 +3,14 @@
 vcfunp="$1"
 vcfp="$2"
 
+# Site quality
 vcftools \
     --gzvcf ${vcfunp} \
     --site-quality \
     --out ${vcf/.vcf.gz/}
 
+
+# Long runs of homozygosity
 for chr in {1..14}; do 
 
     vcftools \
@@ -17,3 +20,9 @@ for chr in {1..14}; do
 	--out bipass${chr}
 done
 
+# Tajima's D (Nucleotide diversity and selection)
+vcftools \
+    --gzvcf pass_biseg_core_Pf3D7_all_v3_updated.vcf.gz \
+    --TajimaD 50 \
+    --maf 0.05 \
+    --out bipaseg
