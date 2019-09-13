aad405c7e649cc7ff04b017911c33613a7608cf7
esohkevin <esohkevin4@gmail.com>
2019-09-13 10:21:10 +0300

    add moi scripts

--- fws/exclude_sites.sh
+++ fws/exclude_sites.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-bcftools view -t ^2:105800,10:68970,12:2163700 -Oz -o new.vcf.gz all.vcf.gz
-bcftools index --tbi new.vcf.gz
+bcftools view -S pca.ids -t ^2:105800,10:68970,12:2163700 -Oz -o moi.vcf.gz ../raw/hqcds_bi_Pf3D7_all_v3_updated.vcf.gz
+bcftools index -f --tbi moi.vcf.gz
