7937da8a9a477c169f7b2b552449ab2ce3851db2
esohkevin <esohkevin4@gmail.com>
2019-09-15 00:43:19 +0300

    update ldhat scripts

--- ldh/run_pairwise.sh
+++ ldh/run_pairwise.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-if [[ $# == 1 ]]; then
+if [[ $# == 2 ]]; then
+
+    base="$2"
 
     for chr in {1..22}; do 
-	echo "-seq chr${chr}.ldhat.sites -loc chr${chr}.ldhat.locs -prefix chr${chr}"; 
+	echo "-seq ${base}chr${chr}.ldhat.sites -loc ${base}chr${chr}.ldhat.locs -prefix ${base}chr${chr}";
     done > arg_file.txt
 
-cat arg_file.txt | xargs -P22 -n6 pairwise
+cat arg_file.txt | xargs -P11 -n6 pairwise
 
 #pairwise -seq chr10.ldhat.sites -loc chr10.ldhat.locs -prefix chr10
 else
     echo """
-	Usage: ./run_pairwise.sh <#chr>
+	Usage: ./run_pairwise.sh <#chr> <.sites+.locs-prefix>
     """
 fi
