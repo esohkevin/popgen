b6de54d6b26383e2bb10a9129ec93494c595d145
esohkevin <esohkevin4@gmail.com>
2019-12-31 06:50:52 +0300

    update

--- myLD/ldCalc.sh
+++ myLD/ldCalc.sh
@@ -13,53 +13,58 @@
 
 vcf2use="$1"
 #vcf2use=args[1]
+s=$2
 
 ########################
 ## CP POINT ESTIMATES ##
 ########################
-
-for i in {1..14}
-do
-
-## split vcf
-vcftools --gzvcf $vcf2use \
-	--recode \
-	--maf 0.05 \
-	--out chr$i
-
-## calculate LD
-vcftools --gzvcf chr$i.recode.vcf \
-	--hap-r2 \
-	--ld-window-bp 100000 \
-	--out chr$i.ld.1-100000
-
-done
-
-
-########################
-####### BOOTSTRAP ######
-########################
-
-for strap in {1..100} # number of bootstraps to do
-do
-
-	## sample without replacement
-	## sampling with replacement is impossible with vcftools
-	## pull 17 isolates
-	shuf -n17 sample/samples.txt >> bootstrap/boot/boot$strap.txt
-
-	## subsample the VCF
-	vcftools --gzvcf $vcf2use \
-		--keep bootstrap/boot/boot$strap.txt \
-		--recode \
-		--out bootstrap/vcfs/boot$strap
-
-	## calculate LD stats
-	vcftools --gzvcf bootstrap/vcfs/boot$strap.recode.vcf \
-		--hap-r2 \
-		--ld-window-bp 100000 \
-		--out bootstrap/ld/boot$strap.ld.1-100000
-
-echo $strap
-done
-
+if [[ $# != 2 ]]; then
+   echo """
+   Usage: ldCalc.sh <in-vcf> <sample-file>
+   """
+else
+   for i in {1..14}
+   do
+   
+   ## split vcf
+   vcftools --gzvcf $vcf2use \
+   	--recode \
+   	--maf 0.05 \
+   	--out chr$i
+   
+   ## calculate LD
+   vcftools --gzvcf chr$i.recode.vcf \
+   	--hap-r2 \
+   	--ld-window-bp 100000 \
+   	--out chr$i.ld.1-100000
+   
+   done
+   
+   
+   ########################
+   ####### BOOTSTRAP ######
+   ########################
+   
+   for strap in {1..100} # number of bootstraps to do
+   do
+   
+   	## sample without replacement
+   	## sampling with replacement is impossible with vcftools
+   	## pull 17 isolates
+   	shuf -n17 $s >> bootstrap/boot/boot$strap.txt
+   
+   	## subsample the VCF
+   	vcftools --gzvcf $vcf2use \
+   		--keep bootstrap/boot/boot$strap.txt \
+   		--recode \
+   		--out bootstrap/vcfs/boot$strap
+   
+   	## calculate LD stats
+   	vcftools --gzvcf bootstrap/vcfs/boot$strap.recode.vcf \
+   		--hap-r2 \
+   		--ld-window-bp 100000 \
+   		--out bootstrap/ld/boot$strap.ld.1-100000
+   
+   echo $strap
+   done
+fi
